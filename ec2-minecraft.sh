4d9aa76865ecfbe6986be360dd4a3679a9363354
Robert Buren <robert@buren.se>
2017-05-17 20:50:45 +0200

    added a missing sudo. updated comments

--- ec2-minecraft.sh
+++ ec2-minecraft.sh
@@ -2,8 +2,10 @@
 # this is for the Amazon Linux AMI. Note that port 25565 must be added to the security group used.
 #
 # instructions copied from http://tsmonaghan.com/set-minecraft-server-aws-cloud-complete-guide-87
+# to use, put this in the "user data" field when creating a new EC2 instance:
+# curl https://raw.githubusercontent.com/robu/ec2-startup-scripts/master/ec2-minecraft.sh | bash
 
-yum update -y
+sudo yum update -y
 mkdir minecraft
 cd minecraft
 wget `curl https://minecraft.net/en-us/download/server | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep https://s3.amazonaws.com/Minecraft.Download`
