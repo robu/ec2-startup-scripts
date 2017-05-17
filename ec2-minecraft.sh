c5734c027b9dd3cbe46ae3b93897f62f38958ce6
Robert Buren <robert@buren.se>
2017-05-17 20:39:16 +0200

    added ec2-minecraft.sh

---
+++ ec2-minecraft.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# this is for the Amazon Linux AMI. Note that port 25565 must be added to the security group used.
+#
+
+yum update -y
+mkdir minecraft
+cd minecraft
+wget `curl https://minecraft.net/en-us/download/server | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep https://s3.amazonaws.com/Minecraft.Download`
+ln -s mine* minecraft_server.jar
+
+# this will return an error, but also create the eula.txt that we need to change:
+sudo java -Xmx1024M -jar minecraft_server.jar nogui
+
+sudo chmod a+rw eula.txt
+echo "eula=true" > eula.txt
+sudo chmod go-w eula.txt
+
+screen sudo java -Xmx1024M -jar minecraft_server.jar nogui
