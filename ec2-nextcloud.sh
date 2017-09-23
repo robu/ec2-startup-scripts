7c3118c4150aef01883136979b254ba2b1a88088
Robert Buren <robert@buren.se>
2017-09-24 00:30:57 +0200

    ec2-nextcloud.sh: added some php modules

--- ec2-nextcloud.sh
+++ ec2-nextcloud.sh
@@ -7,8 +7,10 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 
 sudo apt-get install apache2 -yq
-sudo apt-get install php php-gd libapache2-mod-php sqlite php-sqlite3 -yq
+sudo apt-get install php php-gd libapache2-mod-php sqlite -yq
+sudo apt-get install php-sqlite3 php-mysql -yq
 sudo apt-get install php-zip php-mbstring php-fdomdocument php-curl -yq
+sudo apt-get install php-bz2 php-mcrypt php-imagick php-memcached php-gmp -yq
 sudo apt-get install unzip -yq
 
 sudo service apache2 restart
